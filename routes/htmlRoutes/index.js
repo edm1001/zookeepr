9fdd48651c7f1f53932b0d4614ac149c5893ddf5
edm1001 <edmer_franciz@yahoo.com>
2023-08-27 13:28:30 -0500

    create test dir

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,20 +3,20 @@ const path = require('path');
 
 // home page
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
   });
 // animals page
 router.get('/animals', (req, res)=> {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
   });
 
 // caretakerpage
 router.get('/zookeepers', (req, res)=> {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
   });
 
 router.get('*', (req,res) => {
-    res.send(path.join(__dirname, './public/index.html'));
+    res.send(path.join(__dirname, '../../public/index.html'));
   });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes')
 
 
 const PORT = process.env.PORT || 3001;
@@ -12,10 +12,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-
-
-
-
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
