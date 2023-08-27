d7fee7d695d66061679052095962cdc3194398a8
edm1001 <edmer_franciz@yahoo.com>
2023-08-27 16:07:02 -0500

    create zookepers funct

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes.js');
+const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes.js');
 
 router.use(animalRoutes);
+router.use(zookeeperRoutes);
 
 module.exports = router;
